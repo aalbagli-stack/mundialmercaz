d4794dbfa80923c678abefa7e3302556dec27e4f
Andressanmartin21 <163340896+Andressanmartin21@users.noreply.github.com>
2026-05-08 18:22:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -9,3 +9,4 @@ const SUPABASE_CONFIG = {
     url: 'https://rbsbuysvzqhfmthdzqsv.supabase.co',
     publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJic2J1eXN2enFoZm10aGR6cXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTYwNjMsImV4cCI6MjA5MzgzMjA2M30.4oxfkIOafc4Kqe07_LurQDpDfhcygwlf2pMdjKd-nPo'
 };
+
